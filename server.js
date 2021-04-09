87ae42d3e64faa87215c76ddfac119e2b76879e3
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-04-09 14:04:37 -0400

    s

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const app = express();
+const DB = require("./config/keys")
+
 const mongoose = require("mongoose");
 
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
