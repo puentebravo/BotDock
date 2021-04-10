f61e813e247d330033eb43c0afb404f66e34e802
Dominique Christopher <showlandproductions@gmail.com>
2021-04-10 02:02:41 -0700

    Updated index.js file

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import reportWebVitals from './reportWebVitals';
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
 registerServiceWorker();
