b68833f5d5310a5fb8d12fcfd9c0531a68293979
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-04-15 19:31:13 -0400

    fixed controller file

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,23 +15,23 @@ create: function(req, res) {
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Book
+    db.User
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Book
+    db.User
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Book
+    db.User
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
-  }
+  },
 
 }
