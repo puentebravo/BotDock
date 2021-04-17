5dee41ff02d6559e7851753c3f969998e18bdf5d
Puentebravo <puentematos@gmail.com>
2021-04-17 11:41:29 -0400

    Added bot initialization code

--- bots/stockCheck/index.js
+++ bots/stockCheck/index.js
@@ -3,4 +3,19 @@ require("dotenv").config();
 const puppeteer = require("puppeteer");
 const config = require("./config.json");
 const sites = require("./sites.json");
+const { pingSite } = require("./src/pingsite");
 
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+
+const deploy = async () => {
+  try {
+    for (let index = 0; index < sites.length; index += 1) {
+      await pingSite(sites[index], page);
+    }
+  } finally {
+      await browser.close();
+  }
+};
+
+deploy(); 
