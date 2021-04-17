6692f0aa4772854c27272baf52f9d935401e7ff1
Puentebravo <puentematos@gmail.com>
2021-04-17 11:43:15 -0400

    added async function, DRYed out code

--- bots/stockCheck/index.js
+++ bots/stockCheck/index.js
@@ -5,17 +5,17 @@ const config = require("./config.json");
 const sites = require("./sites.json");
 const { pingSite } = require("./src/pingsite");
 
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
-
 const deploy = async () => {
+  const browser = await puppeteer.launch(config);
+  const page = await browser.newPage();
+
   try {
     for (let index = 0; index < sites.length; index += 1) {
       await pingSite(sites[index], page);
     }
   } finally {
-      await browser.close();
+    await browser.close();
   }
 };
 
-deploy(); 
+deploy();
