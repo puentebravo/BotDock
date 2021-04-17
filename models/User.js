3d5f20e4064913047a8fcf3ba67e9e450c6c4ede
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-04-17 11:36:22 -0400

    Added phone number to db model

--- models/User.js
+++ models/User.js
@@ -6,26 +6,37 @@ const UserSchema = new Schema({
   userName: {
     type: String,
     trim: true,
-    required: "Username is Required"
+    required: "Username is Required",
   },
 
   password: {
     type: String,
     trim: true,
     required: "Password is Required",
-    validate: [({ length }) => length >= 6, "Password should be longer."]
+    validate: [({ length }) => length >= 6, "Password should be longer."],
   },
 
   email: {
     type: String,
     unique: true,
-    match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+  },
+
+  phone: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /\d{3}-\d{3}-\d{4}/.test(v);
+      },
+      message: '{VALUE} is not a valid phone number!'
+    },
+    required: [true, 'User phone number required']
   },
 
   userCreated: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
 const User = mongoose.model("User", UserSchema);
