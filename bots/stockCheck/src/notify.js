0ecb98b718ef1681dbb093ee0480c40db6790b1f
Caroline Lynch <carolineablynch@gmail.com>
2021-04-18 20:25:46 -0400

    writing consts for twilio

--- bots/stockCheck/src/notify.js
+++ bots/stockCheck/src/notify.js
@@ -1,1 +1,7 @@
+// See: https://www.twilio.com/docs/sms/tutorials/how-to-send-sms-messages-node-js
 const twilio = require("twilio");
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const phoneNumber = process.env.TWILIO_PHONE_NUMBER;
+const client = twilio(accountSid, authToken);
