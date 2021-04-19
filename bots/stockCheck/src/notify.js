7f850c50f0694f3e64c16607be0ff40ec2374230
Caroline Lynch <carolineablynch@gmail.com>
2021-04-18 20:26:44 -0400

    building notify function

--- bots/stockCheck/src/notify.js
+++ bots/stockCheck/src/notify.js
@@ -5,3 +5,17 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const phoneNumber = process.env.TWILIO_PHONE_NUMBER;
 const client = twilio(accountSid, authToken);
+
+// returns a Promise that resolves with a Message Object
+// See: https://www.twilio.com/docs/sms/api/message-resource#message-properties
+// usage:
+// notify({ body: "Body of Message", to: "+15555555555" })
+// .then(message => console.log(message));
+//
+function notify({ body, to }) {
+    return client.messages.create({
+      body,
+      to,
+      from: phoneNumber
+    });
+  }
