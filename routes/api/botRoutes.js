8deb5e7c6ed15b4149fe0a2e6a5af80591bfd327
Puentebravo <puentematos@gmail.com>
2021-04-22 19:47:26 -0400

    added bot api route

--- routes/api/botRoutes.js
+++ routes/api/botRoutes.js
@@ -1,6 +1,9 @@
 const router = require ("express").Router();
 const stockCheck = require ("../../bots/stockCheck/index")
 
-router.get("/stockCheck", stockCheck);
+router.get("/stockCheck", (req, res) => {
+    stockCheck();
+    res.send("Deployment initiated.")
+})
 
 module.exports = router
