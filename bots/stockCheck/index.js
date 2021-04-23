78341bd4323ed2226eb793148ebedb97ab7be3b0
Puentebravo <puentematos@gmail.com>
2021-04-22 21:47:53 -0400

    added module export for use with api routing

--- bots/stockCheck/index.js
+++ bots/stockCheck/index.js
@@ -3,12 +3,15 @@ const config = require("./config.json");
 const sites = require("./sites.json");
 const { pingSite } = require("./src/pingsite");
 
+require("dotenv").config();
+
+console.log(process.env)
 const deploy = async () => {
   const browser = await puppeteer.launch(config);
   const page = await browser.newPage();
 
   console.log("Initiating Search...");
-  
+
   try {
     for (let index = 0; index < sites.length; index += 1) {
       await pingSite(sites[index], page);
@@ -18,6 +21,6 @@ const deploy = async () => {
   }
 };
 
-deploy();
+// deploy();
 
-// module.exports = deploy
+module.exports = deploy
