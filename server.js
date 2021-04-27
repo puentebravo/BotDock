e35a2828d28c68afa1ce291cc52c2fde4f74c3c3
Puentebravo <puentematos@gmail.com>
2021-04-27 16:28:38 -0400

    Augmented app.use serving middleware to fix deployment

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./client/public"));
+  app.use('/static', express.static(path.join(__dirname, 'client/build')));
 }
 
 // Send every request to the React app
