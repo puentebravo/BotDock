cce944c97f8f7da019d1f69028863ceb625d037a
Puentebravo <puentematos@gmail.com>
2021-04-27 15:29:02 -0400

    Imported API, wrote click function to deploy bot

--- client/src/pages/Directory.js
+++ client/src/pages/Directory.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import API from "../utils/API.js";
 
 const bots = [
   {
@@ -6,18 +7,38 @@ const bots = [
     description: "Trouble finding the newest Xbox? This bot is for you!",
   },
   { name: "Bot #2", description: "This is Bot #2. Aren't they cute? Say Hi!" },
-  { name: "Bot #3", description: "This is Bot #3. Bot #3 is a bit confused. Say Hi!" },
-  { name: "Bot #4", description: "This is Bot #4. Bot #4 has places to go. Say Hi!" },
+  {
+    name: "Bot #3",
+    description: "This is Bot #3. Bot #3 is a bit confused. Say Hi!",
+  },
+  {
+    name: "Bot #4",
+    description: "This is Bot #4. Bot #4 has places to go. Say Hi!",
+  },
 ];
 
 function Index() {
   const [selectedBot, setSelectedBot] = useState(0);
 
+  const deployBot = () => {
+    API.getBot().then(() => {
+      alert(
+        "Bot Deployed! Be sure to check your SMS messages. If you don't hear from your bot in the next few minutes, make sure the phone number we have on file is correct."
+      );
+    });
+  };
+
   return (
     <div style={{ minHeight: "100%" }}>
-
-      <div className="bg-gray-200 pb-10" style={{paddingBottom:"100%", marginTop:"-2%", position:"relative"}}>
-        <div className="container px-6 mx-auto" style={{paddingTop:"8%"}}>
+      <div
+        className="bg-gray-200 pb-10"
+        style={{
+          paddingBottom: "100%",
+          marginTop: "-2%",
+          position: "relative",
+        }}
+      >
+        <div className="container px-6 mx-auto" style={{ paddingTop: "8%" }}>
           <div className="relative z-10 w-full">
             <div className="w-full -mt-8 h-auto">
               {/* Remove class [ h-64 ] when adding a card block */}
@@ -25,11 +46,17 @@ function Index() {
                 className="container mx-auto h-64"
                 style={{ height: "1000%" }}
               >
-                  <p style={{textAlign:"center", fontSize:"25px", marginTop:"-2%"}}>Choose Your Bot Below</p>
+                <p
+                  style={{
+                    textAlign: "center",
+                    fontSize: "25px",
+                    marginTop: "-2%",
+                  }}
+                >
+                  Choose Your Bot Below
+                </p>
                 <div className="bg-white w-full h-full rounded shadow">
-                    
                   <div className="grid grid-cols-2 gap-4">
-                      
                     <div
                       className="border-solid border-4 h-48 overflow-scroll"
                       style={{ height: "100%" }}
@@ -109,21 +136,28 @@ function Index() {
                       style={{ font: "menu", fontSize: "18px" }}
                     >
                       <p className="text-center mb-8"></p>
-                      
-                        <p style={{textAlign:"center", marginTop:"-4%"}}>Name:</p>
-                        <br></br>
-                        <div style={{textAlign:"center"}}>
+
+                      <p style={{ textAlign: "center", marginTop: "-4%" }}>
+                        Name:
+                      </p>
+                      <br></br>
+                      <div style={{ textAlign: "center" }}>
                         {bots[selectedBot].name}
-                        </div>
+                      </div>
                       <div>
                         <div className="p-8 border-solid border-4">
-                          <p style={{textAlign:"center", marginTop:"-4%"}}>Description:</p>
+                          <p style={{ textAlign: "center", marginTop: "-4%" }}>
+                            Description:
+                          </p>
                           <br></br>
-                          <div style={{textAlign:"center"}}>
-                          {bots[selectedBot].description}
+                          <div style={{ textAlign: "center" }}>
+                            {bots[selectedBot].description}
                           </div>
                           <div className="text-center">
-                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-16">
+                            <button
+                              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-16"
+                              onClick={deployBot()}
+                            >
                               Deploy
                             </button>
                             <br></br>
