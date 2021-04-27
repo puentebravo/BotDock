daa7babe5493fc98ecfd8ef15ef266bbc92792ec
Puentebravo <puentematos@gmail.com>
2021-04-26 21:04:05 -0400

    Removed deprecated imports, cleaned up formatting

--- client/src/App.js
+++ client/src/App.js
@@ -1,63 +1,30 @@
-import React, { Component, useState, useEffect, } from "react";
+import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Contact from "./pages/Contact";
 import Directory from "./pages/Directory";
 import NavBar from "./components/NavBar";
 import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
-import { AuthState, onAuthUIStateChange  } from "@aws-amplify/ui-components";
 import {
   withAuthenticator,
-  AmplifySignIn,
   AmplifySignOut,
   AmplifyAuthenticator,
 } from "@aws-amplify/ui-react";
-//import logo from './logo.svg';
 import "./App.css";
-
 
 Amplify.configure(awsconfig);
 
-
-
-
-
-
-  
-
-
-
-
-    
-
 function App() {
-  function handleAuthStateChange (nextAuthState) {
-  if (nextAuthState === AuthState.SignedIn) {
-    window.location.href = ("/Directory")
-    
-  }
-}
-
-
   return (
-    
-      <AmplifyAuthenticator >
+    <AmplifyAuthenticator>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <NavBar />
-        
-          {/* <Route exact path="/" component={AmplifySignIn} /> */}
-
-          <Route exact path="/" component={Directory} />
-          <Route exact path="/Contact" component={Contact} />
+        <Route exact path="/" component={Directory} />
+        <Route exact path="/Contact" component={Contact} />
       </BrowserRouter>
-      <AmplifySignOut/>
-      </AmplifyAuthenticator>
-    );
-   
+      <AmplifySignOut />
+    </AmplifyAuthenticator>
+  );
 }
 
-
-
-
 export default withAuthenticator(App);
-
